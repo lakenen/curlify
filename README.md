2479ccc9a8f5dbe1f267187acfdf5c7ed0dc5610
Cameron Lakenen <lakenen@box.com>
2014-07-22 00:31:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# curlify
+
+Transform a [cURL](http://curl.haxx.se/) command into a node function that will make the request specified by cURL command line arguments.
+
+## Usage
+
+Example
+```js
+var curlify = require('curlify')
+// generate a request method from this cURL command string
+var request = curlify('curl -X POST -H "content-type: application/json" -d \'{"foo": "bar"}\' http://example.com')
+// make an http POST with the specified headers and data
+request().pipe(process.stdout)
+```
+
+
+## License
+
+([The MIT License](LICENSE))
+
+Copyright 2014 Cameron Lakenen
---
+++ index.js
@@ -0,0 +1,82 @@
+var argify = require('spawn-args')
+  , minimist = require('minimist')
+  , request = require('hyperquest')
+  , extend = require('extend')
+
+module.exports = function (str) {
+  var argv = minimist(argify(str))
+    , url = argv._[1]
+    , options = {
+      headers: {}
+    }
+    , data = ''
+
+  Object.keys(argv).forEach(function (k) {
+    switch (k) {
+      case 'H':
+        addHeaders(argv[k])
+        break
+      case 'd':
+        addData(argv[k])
+        break
+      case 'X':
+        addOption('method', argv[k])
+        break
+    }
+  })
+
+  return function (u, opt, dat) {
+    u = u || url
+    opt = extend(true, {}, options, opt || {})
+    dat = dat || data
+    return makeRequest(u, opt, dat)
+  }
+
+  function addHeaders(harr) {
+    if (!Array.isArray(harr)) {
+      harr = [harr]
+    }
+    harr.map(unquote).map(function (h) {
+      return h.split(/:\s*/)
+    }).forEach(addHeader)
+  }
+
+  function addHeader(h) {
+    options.headers[h[0]] = h[1]
+  }
+
+  function addOption(name, val) {
+    options[name] = val
+  }
+
+  function addData(d) {
+    if (Array.isArray(d)) {
+      data = d.reduce(function (x, d) {
+        return x + '&' + unquote(d)
+      })
+    } else {
+      data = unquote(d)
+    }
+  }
+
+  function makeRequest(url, opt, data) {
+    var req = request(url, opt)
+
+    if (data) {
+      req.setHeader('content-length', data.length)
+      req.end(data)
+    }
+    return req
+  }
+}
+
+function unquote(str) {
+  var q = /[\'\"]/
+  if (q.test(str.charAt(0))) {
+    str = str.substr(1)
+  }
+  if (q.test(str.charAt(str.length - 1))) {
+    str = str.substr(0, str.length - 1)
+  }
+  return str
+}
